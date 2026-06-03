031533ee47d662a95f06efdef0bcccdd336cda4a
Shawn C. Wright <swright@waveframelabs.org>
2026-06-03 07:01:33 -0400

    Removed commercial assets from public layer

--- temp_core/governance_ledger/projection.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility imports for local registry projections."""
-
-from governance_ledger.local_registry.projections.workspace import (
-    AUTHORITY_WORKSPACE_PROJECTION_V1,
-    build_authority_workspace_projection,
-)
-
-__all__ = ["AUTHORITY_WORKSPACE_PROJECTION_V1", "build_authority_workspace_projection"]
