002a6dbc1686bda99113f63d24b25bd6bd26b863
Shawn C. Wright <swright@waveframelabs.org>
2026-07-13 23:14:13 -0400

    Final release pass for v0.5.0

--- governance_ledger/__init__.py
+++ governance_ledger/__init__.py
@@ -9,7 +9,7 @@ from governance_ledger.inspect import format_artifact, format_contract_list, lis
 from governance_ledger.lifecycle import transition_review_status
 from governance_ledger.provenance import build_review_provenance
 from governance_ledger.publish import approve_review_file, publish_review_file
-from governance_ledger.registry import load_contract_registry, update_contract_registry
+from governance_ledger.registry import load_contract_registry, resolve_authority_ref, update_contract_registry
 from governance_ledger.report import review_constraints, validate_constraints
 from governance_ledger.review import build_review_report
 from governance_ledger.rollback import rollback_to_snapshot
@@ -17,9 +17,9 @@ from governance_ledger.runner import run_policy_directory, run_policy_file
 from governance_ledger.semantics.diff import build_authority_diff_impact
 from governance_ledger.semantics.packets import build_governance_review_packet
 from governance_ledger.semantics.preview import build_governance_impact_preview
-from governance_ledger.semantics.publication import build_authority_bundle
+from governance_ledger.semantics.publication import build_authority_bundle, build_publication_receipt
 from governance_ledger.snapshot import create_snapshot
-from governance_ledger.summary import build_pr_summary, format_publish_summary, format_run_summary
+from governance_ledger.summary import build_pr_summary, format_publish_summary, format_resolution_summary, format_run_summary
 from governance_ledger.validation import validate_authoring, validate_compiler_policy
 
 __all__ = [
@@ -32,6 +32,7 @@ __all__ = [
     "build_governance_impact_preview",
     "build_authority_diff_impact",
     "build_authority_bundle",
+    "build_publication_receipt",
     "build_governance_review_packet",
     "check_validation_directory",
     "create_snapshot",
@@ -40,11 +41,13 @@ __all__ = [
     "format_artifact",
     "format_contract_list",
     "format_publish_summary",
+    "format_resolution_summary",
     "format_run_summary",
     "format_check_summary",
     "list_contracts",
     "load_contract_registry",
     "publish_review_file",
+    "resolve_authority_ref",
     "review_constraints",
     "rollback_to_snapshot",
     "run_policy_directory",
